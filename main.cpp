8166fccdfacb8500e9c2f17496a59dfd1c1c627a
Thino Ivarsson <thinoivarsson7@gmail.com>
2026-08-23 02:44:07 +0200

    feat: triangle fuck yeah

--- main.cpp
+++ main.cpp
@@ -14,8 +14,7 @@ struct SwapChain {
     std::vector<VkImageView> views = {};
 };
 
-bool initVulkan(VkInstance& instance) {
-
+bool initVulkan(VkInstance &instance) {
     // Tells the driver which vulkan version we are using
     VkApplicationInfo app{};
     app.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -23,7 +22,7 @@ bool initVulkan(VkInstance& instance) {
 
     // Needed to allow vulkan to interact with the window
     uint32_t extCount = 0;
-    const char** exts = glfwGetRequiredInstanceExtensions(&extCount);
+    const char **exts = glfwGetRequiredInstanceExtensions(&extCount);
 
     // The "recipe" for our vulkan Instance
     VkInstanceCreateInfo ci{};
@@ -34,8 +33,8 @@ bool initVulkan(VkInstance& instance) {
 
     // Enables debug for vulkan. ifndef makes sure to only enable if debugging since it impacts performance
 #ifndef NDEBUG
-    const char* layers[] = { "VK_LAYER_KHRONOS_validation" };
-    ci.enabledLayerCount   = 1;
+    const char *layers[] = {"VK_LAYER_KHRONOS_validation"};
+    ci.enabledLayerCount = 1;
     ci.ppEnabledLayerNames = layers;
 #endif
 
@@ -49,7 +48,7 @@ bool initVulkan(VkInstance& instance) {
     return true;
 }
 
-bool pickGpu(VkInstance& instance, VkPhysicalDevice& gpu) {
+bool pickGpu(VkInstance &instance, VkPhysicalDevice &gpu) {
     // Finds all gpus
     // call two times, once to get the amount and once to fill an array.
     uint32_t deviceCount = 0;
@@ -59,12 +58,15 @@ bool pickGpu(VkInstance& instance, VkPhysicalDevice& gpu) {
     vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
     // Sorts through devices to find one that is an actual gpu
-    for (auto& device : devices) {
+    for (auto &device: devices) {
         VkPhysicalDeviceProperties props;
         vkGetPhysicalDeviceProperties(device, &props);
         printf("Device name: %s\n", props.deviceName);
 
-        if (props.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {gpu = device; return true;}
+        if (props.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+            gpu = device;
+            return true;
+        }
     }
 
     printf("Failed to find suitable GPU!\n");
@@ -72,7 +74,7 @@ bool pickGpu(VkInstance& instance, VkPhysicalDevice& gpu) {
 }
 
 //? returns the index of the queue because the queue doesn't exist yet
-bool findGraphicsQueueFamilyIndex(VkPhysicalDevice& gpu, uint32_t& queueFamilyIndex, VkSurfaceKHR& surface) {
+bool findGraphicsQueueFamilyIndex(VkPhysicalDevice &gpu, uint32_t &queueFamilyIndex, VkSurfaceKHR &surface) {
     // find gpu queue families
     // Run twice, same as in pickGpu()
     uint32_t familyCount = 0;
@@ -86,13 +88,17 @@ bool findGraphicsQueueFamilyIndex(VkPhysicalDevice& gpu, uint32_t& queueFamilyIn
         bool canDraw = families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT;
         VkBool32 canPresent = VK_FALSE;
         vkGetPhysicalDeviceSurfaceSupportKHR(gpu, i, surface, &canPresent);
-        if (canDraw && canPresent) {printf("Graphics queue family index: %u\n", i); queueFamilyIndex = i; return true;}
+        if (canDraw && canPresent) {
+            printf("Graphics queue family index: %u\n", i);
+            queueFamilyIndex = i;
+            return true;
+        }
     }
     printf("Could not find graphics queue family index!\n");
     return false;
 }
 
-bool createLogicalDevice(uint32_t queueFamilyIndex, VkPhysicalDevice& gpu, VkDevice& device, VkQueue& graphicsQueue) {
+bool createLogicalDevice(uint32_t queueFamilyIndex, VkPhysicalDevice &gpu, VkDevice &device, VkQueue &graphicsQueue) {
     float priority = 1.0f;
 
     // The "recipe" for the device queue
@@ -108,7 +114,7 @@ bool createLogicalDevice(uint32_t queueFamilyIndex, VkPhysicalDevice& gpu, VkDev
     features.dynamicRendering = VK_TRUE;
     features.synchronization2 = VK_TRUE;
 
-    const char* deviceExtensions[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+    const char *deviceExtensions[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
     // The "recipe" for the device
     VkDeviceCreateInfo createInfo{};
@@ -131,8 +137,7 @@ bool createLogicalDevice(uint32_t queueFamilyIndex, VkPhysicalDevice& gpu, VkDev
     return true;
 }
 
-bool createSwapchain(VkPhysicalDevice& gpu, VkSurfaceKHR& surface, VkDevice& device, SwapChain& out) {
-
+bool createSwapchain(VkPhysicalDevice &gpu, VkSurfaceKHR &surface, VkDevice &device, SwapChain &out) {
     //! GET INFORMATION ABOUT THE SWAPCHAIN
     // Get some information Idk
     VkSurfaceCapabilitiesKHR caps;
@@ -146,7 +151,7 @@ bool createSwapchain(VkPhysicalDevice& gpu, VkSurfaceKHR& surface, VkDevice& dev
 
     // Get the correct format
     VkSurfaceFormatKHR chosenFormat = formats[0];
-    for (const auto& format : formats) {
+    for (const auto &format: formats) {
         if (format.format == VK_FORMAT_R8G8B8A8_SRGB &&
             format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
             chosenFormat = format;
@@ -205,7 +210,8 @@ bool createSwapchain(VkPhysicalDevice& gpu, VkSurfaceKHR& surface, VkDevice& dev
         ici.image = out.images[i];
         ici.viewType = VK_IMAGE_VIEW_TYPE_2D;
         ici.format = out.format;
-        ici.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT; // Color data as opposed to something like a depth buffer
+        ici.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        // Color data as opposed to something like a depth buffer
         ici.subresourceRange.baseMipLevel = 0;
         ici.subresourceRange.levelCount = 1;
         ici.subresourceRange.baseArrayLayer = 0;
@@ -221,7 +227,7 @@ bool createSwapchain(VkPhysicalDevice& gpu, VkSurfaceKHR& surface, VkDevice& dev
     return true;
 }
 
-static std::vector<char> readFile(const std::string& path) {
+static std::vector<char> readFile(const std::string &path) {
     // Get the file
     std::ifstream file(path, std::ios::ate | std::ios::binary);
     if (!file.is_open()) {
@@ -230,7 +236,7 @@ static std::vector<char> readFile(const std::string& path) {
     }
 
     // Get the size of the file
-    size_t fileSize = (size_t)file.tellg();
+    size_t fileSize = (size_t) file.tellg();
     std::vector<char> buffer(fileSize);
 
     // Return the file contents
@@ -239,11 +245,11 @@ static std::vector<char> readFile(const std::string& path) {
     return buffer;
 }
 
-static VkShaderModule createShaderModule(VkDevice& device, const std::vector<char>& code) {
+static VkShaderModule createShaderModule(VkDevice &device, const std::vector<char> &code) {
     VkShaderModuleCreateInfo ci{};
     ci.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
     ci.codeSize = code.size();
-    ci.pCode = reinterpret_cast<const uint32_t*>(code.data());
+    ci.pCode = reinterpret_cast<const uint32_t *>(code.data());
 
     VkShaderModule shaderModule;
     if (vkCreateShaderModule(device, &ci, nullptr, &shaderModule) != VK_SUCCESS) {
@@ -254,14 +260,13 @@ static VkShaderModule createShaderModule(VkDevice& device, const std::vector<cha
 }
 
 int main() {
-
     glfwInit();
 
     // finds monitors and hooks to the os type shi
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
     // Creates a window >_<
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Caelus", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Caelus", nullptr, nullptr);
 
     // Initialises vulkan and gives us our instance
     VkInstance instance;
@@ -331,7 +336,8 @@ int main() {
     multisampleInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
 
     VkPipelineColorBlendAttachmentState blendAttachment{};
-    blendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    blendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
+                                     VK_COLOR_COMPONENT_A_BIT;
     blendAttachment.blendEnable = VK_FALSE;
 
     VkPipelineColorBlendStateCreateInfo colorBlendingInfo{};
@@ -339,7 +345,7 @@ int main() {
     colorBlendingInfo.attachmentCount = 1;
     colorBlendingInfo.pAttachments = &blendAttachment;
 
-    VkDynamicState dynamics[] = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR };
+    VkDynamicState dynamics[] = {VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR};
     VkPipelineDynamicStateCreateInfo dynamicInfo{};
     dynamicInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
     dynamicInfo.dynamicStateCount = 2;
@@ -383,9 +389,168 @@ int main() {
 
     //! END OF PIPELINE SHIT
 
+    //! OTHER SHITT
+
+    VkCommandPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    poolInfo.queueFamilyIndex = queueFamilyIndex;
+
+    VkCommandPool commandPool;
+    if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
+        printf("Failed to create command pool!\n");
+    }
+
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool = commandPool;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = 1;
+
+    VkCommandBuffer cmd;
+    if (vkAllocateCommandBuffers(device, &allocInfo, &cmd) != VK_SUCCESS) {
+        printf("Failed to allocate command buffers!\n");
+    }
+
+    VkFenceCreateInfo fenceInfo{};
+    fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+    fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+    VkFence inFlight;
+    if (vkCreateFence(device, &fenceInfo, nullptr, &inFlight) != VK_SUCCESS) {
+        printf("Failed to create fence!\n");
+    }
+
+    VkSemaphoreCreateInfo semaphoreInfo{};
+    semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+    VkSemaphore imageAvailable;
+    if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &imageAvailable) != VK_SUCCESS) {
+        printf("Failed to create semaphore!\n");
+    }
+
+    std::vector<VkSemaphore> renderFinished(swapChain.images.size());
+    for (auto &s: renderFinished) {
+        vkCreateSemaphore(device, &semaphoreInfo, nullptr, &s);
+    }
+
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
+        vkWaitForFences(device, 1, &inFlight, VK_TRUE, UINT64_MAX);
+        vkResetFences(device, 1, &inFlight);
+
+        uint32_t imageIndex;
+        vkAcquireNextImageKHR(device, swapChain.handle, UINT64_MAX, imageAvailable, VK_NULL_HANDLE, &imageIndex);
+
+        vkResetCommandBuffer(cmd, 0);
+
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        vkBeginCommandBuffer(cmd, &beginInfo);
+
+        VkImageMemoryBarrier2 barrier{};
+        barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2;
+        barrier.srcStageMask = VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT;
+        barrier.srcAccessMask = 0;
+        barrier.dstStageMask = VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT;
+        barrier.dstAccessMask = VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT;
+        barrier.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        barrier.newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.image = swapChain.images[imageIndex];
+        barrier.subresourceRange = {VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1};
+
+        VkDependencyInfo dependencyInfo{};
+        dependencyInfo.sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO;
+        dependencyInfo.imageMemoryBarrierCount = 1;
+        dependencyInfo.pImageMemoryBarriers = &barrier;
+
+        vkCmdPipelineBarrier2(cmd, &dependencyInfo);
+
+        VkRenderingAttachmentInfo colorAttachmentInfo{};
+        colorAttachmentInfo.sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO;
+        colorAttachmentInfo.imageView = swapChain.views[imageIndex];
+        colorAttachmentInfo.imageLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+        colorAttachmentInfo.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachmentInfo.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        colorAttachmentInfo.clearValue.color = {0.02f, 0.02f, 0.05f, 1.0f};
+
+        VkRenderingInfo renderInfo{};
+        renderInfo.sType = VK_STRUCTURE_TYPE_RENDERING_INFO;
+        renderInfo.renderArea = {{0,0}, swapChain.extent};
+        renderInfo.layerCount = 1;
+        renderInfo.colorAttachmentCount = 1;
+        renderInfo.pColorAttachments = &colorAttachmentInfo;
+
+        vkCmdBeginRendering(cmd, &renderInfo);
+        vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+
+        VkViewport vp{};
+        vp.width = static_cast<float>(swapChain.extent.width);
+        vp.height = static_cast<float>(swapChain.extent.height);
+        vp.minDepth = 0.0f;
+        vp.maxDepth = 1.0f;
+        vkCmdSetViewport(cmd, 0, 1, &vp);
+
+        VkRect2D scissor{{0,0}, swapChain.extent};
+        vkCmdSetScissor(cmd, 0, 1, &scissor);
+
+        vkCmdDraw(cmd, 3, 1, 0, 0);
+        vkCmdEndRendering(cmd);
+
+        VkImageMemoryBarrier2 toPresent = barrier;
+        toPresent.srcStageMask = VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT;
+        toPresent.srcAccessMask = VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT;
+        toPresent.dstStageMask = VK_PIPELINE_STAGE_2_BOTTOM_OF_PIPE_BIT;
+        toPresent.dstAccessMask = 0;
+        toPresent.oldLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+        toPresent.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        dependencyInfo.pImageMemoryBarriers = &toPresent;
+        vkCmdPipelineBarrier2(cmd, &dependencyInfo);
+
+        vkEndCommandBuffer(cmd);
+
+        VkCommandBufferSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO;
+        submitInfo.commandBuffer = cmd;
+
+        VkSemaphoreSubmitInfo waitInfo{};
+        waitInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO;
+        waitInfo.semaphore = imageAvailable;
+        waitInfo.stageMask = VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT;
+
+        VkSemaphoreSubmitInfo signalInfo{};
+        signalInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO;
+        signalInfo.semaphore = renderFinished[imageIndex];
+        signalInfo.stageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+
+        VkSubmitInfo2 submit{};
+        submit.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO_2;
+        submit.waitSemaphoreInfoCount = 1;
+        submit.pWaitSemaphoreInfos = &waitInfo;
+        submit.commandBufferInfoCount = 1;
+        submit.pCommandBufferInfos = &submitInfo;
+        submit.signalSemaphoreInfoCount = 1;
+        submit.pSignalSemaphoreInfos = &signalInfo;
+
+        vkQueueSubmit2(graphicsQueue, 1, &submit, inFlight);
+
+        VkPresentInfoKHR presentInfo{};
+        presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+        presentInfo.waitSemaphoreCount = 1;
+        presentInfo.pWaitSemaphores = &renderFinished[imageIndex];
+        presentInfo.swapchainCount = 1;
+        presentInfo.pSwapchains = &swapChain.handle;
+        presentInfo.pImageIndices = &imageIndex;
+
+        vkQueuePresentKHR(graphicsQueue, &presentInfo);
     }
+
+        vkDeviceWaitIdle(device);
 
     // Clean everything up
 
@@ -393,8 +558,16 @@ int main() {
     vkDestroyPipeline(device, pipeline, nullptr);
     vkDestroyPipelineLayout(device, layout, nullptr);
 
-    for (VkImageView v : swapChain.views)
+    for (VkImageView v: swapChain.views)
         vkDestroyImageView(device, v, nullptr);
+
+    vkDestroyFence(device, inFlight, nullptr);
+    vkDestroySemaphore(device, imageAvailable, nullptr);
+
+    for (VkSemaphore s: renderFinished)
+        vkDestroySemaphore(device, s, nullptr);
+
+    vkDestroyCommandPool(device, commandPool, nullptr); // frees `cmd` too
 
     vkDestroySwapchainKHR(device, swapChain.handle, nullptr);
     vkDestroyDevice(device, nullptr);
